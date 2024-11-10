51d5679133cde364f98fafa7b5746b7db607b416
Puyong Johnharvey <159794500+HarveyPuyong@users.noreply.github.com>
2024-11-11 00:01:24 +0800

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,9 @@
+import { skillFunctionality } from "./skills.js";
+import { topNav } from "./topnav.js";
+import { animation } from "./animation.js";
+
+animation()
+topNav();
+skillFunctionality();
+
+
